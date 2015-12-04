c398541f85ae2bec30c3c752b5e5c9b6454b78b3
Hosomy17 <jackson.daniel.17dh@gmail.com>
2015-12-03 20:19:27 -0400

    Acertado cone de criação de itens e colição de itens

--- Assets/Source Code/Project/AIManager/ItenSpawner.cs
+++ Assets/Source Code/Project/AIManager/ItenSpawner.cs
@@ -10,7 +10,7 @@ public class ItenSpawner : MonoBehaviour
     {
         Spawn();
     }
-    
+
     void Update ()
     {
         
@@ -21,14 +21,19 @@ public class ItenSpawner : MonoBehaviour
         Vector2 randomPointOnCircle;
         randomPointOnCircle = Random.insideUnitCircle;
         randomPointOnCircle.Normalize();
-
+        
         if (randomPointOnCircle.y < 0)
             randomPointOnCircle.y *= -1;
 
-        aux = Instantiate(iten);
-        aux.transform.position = randomPointOnCircle * 10;
-        aux.transform.position = new Vector2(aux.transform.position.x, aux.transform.position.y-4.5f);
+        if(randomPointOnCircle.x* 15 < -9 || randomPointOnCircle.x* 15 > 9)
+        {
+            Spawn();
+            return;
+        }
 
+        aux = Instantiate(iten);
+        aux.transform.position = randomPointOnCircle * 15;
+        aux.transform.position = new Vector2(aux.transform.position.x, aux.transform.position.y-4.5f);
         aux.GetComponent<Rigidbody2D>().AddForce(-randomPointOnCircle*200);
 
         Invoke("Spawn", 4);
